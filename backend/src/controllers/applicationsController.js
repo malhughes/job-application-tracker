2690fd8553b59549d75baddd710e1a192ddfbf50
Malachi Hughes <hughesmal00@gmail.com>
2025-10-09 18:04:06 -0500

    feat: added setup applications controller and updated model to include timestamps

--- backend/src/controllers/applicationsController.js
+++ backend/src/controllers/applicationsController.js
@@ -1,19 +1,68 @@
-export const getApplications = (req, res) => {
-  res.status(200).send('You got the applicaitons');
-};
+import Application from '../models/Application.js';
 
-export const getApplicationById = (req, res) => {
-  res.status(200).send('You got the applicaiton');
-};
+export async function getApplications(req, res) {
+  try {
+    const applications = await Application.find().sort({ updatedAt: -1 });
+    res.status(200).json(applications);
+  } catch (error) {
+    console.error('Error in getApplications controller', error);
+    res.status(500).json({ message: 'Internal error' });
+  }
+}
 
-export const createApplication = (req, res) => {
-  res.status(200).send('You successfully created an application');
-};
+export async function getApplicationById(req, res) {
+  try {
+    const application = await Application.findById(req.params.id);
 
-export const updateApplication = (req, res) => {
-  res.status(200).send('You successfully updated the application');
-};
+    if (!application) return res.status(404).json({ message: 'Application not found' });
 
-export const deleteApplication = (req, res) => {
-  res.status(200).send('You successfully deleted the application');
-};
+    res.status(200).json(application);
+  } catch (error) {
+    console.error('Error in getApplicationById controller', error);
+    res.status(500).json({ message: 'Internal error' });
+  }
+}
+
+export async function createApplication(req, res) {
+  try {
+    const { title, company, status, nextStep } = req.body;
+    const application = new Application({ title, company, status, nextStep });
+
+    const savedApplication = await application.save();
+    res.status(201).json(savedApplication);
+  } catch (error) {
+    console.error('Error in createApplication controller', error);
+    res.status(500).json({ message: 'Internal error' });
+  }
+}
+
+export async function updateApplication(req, res) {
+  try {
+    const { title, company, status, nextStep } = req.body;
+    const updatedApplication = await Application.findByIdAndUpdate(
+      req.params.id,
+      { title, company, status, nextStep },
+      { new: true }
+    );
+
+    if (!updatedApplication) return res.status(404).json({ message: 'Application not found' });
+
+    res.status(200).json(updatedApplication);
+  } catch (error) {
+    console.error('Error in updateApplication controller', error);
+    res.status(500).json({ message: 'Internal error' });
+  }
+}
+
+export async function deleteApplication(req, res) {
+  try {
+    const deletedApplication = await Application.findByIdAndDelete(req.params.id);
+
+    if (!deletedApplication) return res.status(404).json({ message: 'Application not found' });
+
+    res.status(200).json({ message: 'Application succuessfully deleted' });
+  } catch (error) {
+    console.error('Error in deleteApplication controller', error);
+    res.status(500).json({ message: 'Internal error' });
+  }
+}
--- backend/src/models/Application.js
+++ backend/src/models/Application.js
@@ -1,23 +1,26 @@
 import mongoose from 'mongoose';
 
-const applicationSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const applicationSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    nextStep: {
+      type: String,
+      required: true,
+    },
   },
-  company: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-  nextStep: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Application = mongoose.model('Application', applicationSchema);
 
