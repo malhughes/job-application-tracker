73f98ef55851b6a7c1adddfea34bff7dfebf8063
Malachi Hughes <hughesmal00@gmail.com>
2026-05-08 10:36:17 -0500

    fix: add react route auth protection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import { Dashboard } from './pages/Dashboard';
 import { Signup } from './pages/Signup';
 import { Signin } from './pages/Signin';
 import { Navbar } from './components/Navbar';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <div className="flex h-screen flex-col overflow-hidden">
       <Navbar />
       <div className="flex-1 overflow-auto">
         <Routes>
-          <Route path="/" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={!user ? <Signup /> : <Navigate to="/dashboard" />} />
+          <Route path="/signin" element={!user ? <Signin /> : <Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/signin" />} />
         </Routes>
       </div>
     </div>
