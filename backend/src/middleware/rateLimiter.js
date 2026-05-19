b7a47a1e9edc0ea61d0c145f3deca9a6bbc7f538
Malachi Hughes <hughesmal00@gmail.com>
2026-05-18 21:04:39 -0500

    fix: use user id as rate limit identifier

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -2,7 +2,8 @@ import ratelimit from '../config/upstash.js';
 
 const rateLimiter = async (req, res, next) => {
   try {
-    const { success } = await ratelimit.limit('my-limit-key'); //update identiier with user id when authentication is implemented
+    const identifier = req.user?._id?.toString() ?? req.ip;
+    const { success } = await ratelimit.limit(identifier);
 
     if (!success) {
       return res.status(429).json({
